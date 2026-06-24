9c9aff74457828c83aecfe251f25e5651a0b0ca5
swyam shukla <swyamshukla0104@gmail.com>
2026-06-24 20:48:54 +0530

    week2 work

---
+++ Week2_Spring_Core_and_Maven/week2_9_Creating_a_Spring_Boot_Application/Code/LibraryManagementSystem/src/main/java/com/example/library/entity/Book.java
@@ -0,0 +1,57 @@
+package com.example.library.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String author;
+
+    public Book() {}
+
+    public Book(Integer id, String name, String author) {
+        this.id = id;
+        this.name = name;
+        this.author = author;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }
+}
